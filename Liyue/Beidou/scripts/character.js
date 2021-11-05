b0de21f2dd602d492280cdcb92a4623d44c98686
‘Evan <puzonevan@gmail.com>
2021-11-05 15:27:15 -0700

    image links

--- Liyue/Beidou/scripts/character.js
+++ Liyue/Beidou/scripts/character.js
@@ -101,10 +101,17 @@ function initializeData(){
         if(index < 3){
             collect.innerHTML = `${genshindb["materials"][collectable[index]]["name"]} <strong>x${collectableAmounts[index]}</strong>`;
             collect.previousElementSibling.src = `${genshinimage["materials"][collectable[index]]["fandom"]}`;
+            collect.previousElementSibling.addEventListener('click', () =>{
+                window.open(genshinlink["materials"][collectable[index]]["fandom"], "_blank");
+            });
         } else{
             collect.innerHTML = `${genshindb["materials"][collectable[index - 3]]["name"]} <strong>x${collectableAmounts[index]}</strong>`;
             collect.previousElementSibling.src = `${genshinimage["materials"][collectable[index - 3]]["fandom"]}`;
+            collect.previousElementSibling.addEventListener('click', () =>{
+                window.open(genshinlink["materials"][collectable[index - 3]]["fandom"], "_blank");
+            });
         }
+        
         hover(collect.previousElementSibling);
     });
 
@@ -112,6 +119,10 @@ function initializeData(){
     [...document.getElementsByClassName("unique-collectable")].forEach((unique) =>{
         unique.innerHTML = `${genshindb["materials"][uniqueCollectable]["name"]} <strong>x${uniqueCollectableAmounts}</strong>`;
         unique.previousElementSibling.src = `${genshinimage["materials"][uniqueCollectable]["fandom"]}`;
+
+        unique.previousElementSibling.addEventListener('click', () =>{
+            window.open(genshinlink["materials"][uniqueCollectable]["fandom"], "_blank");
+        });
         hover(unique.previousElementSibling);
     });
 
@@ -119,6 +130,10 @@ function initializeData(){
     [...document.getElementsByClassName("book")].forEach((book, index) =>{
         book.innerHTML = `${genshindb["materials"][books[index]]["name"]} <strong>x${booksAmounts[index]}</strong>`;
         book.previousElementSibling.src = `${genshinimage["materials"][books[index]]["fandom"]}`;
+
+        book.previousElementSibling.addEventListener('click', () =>{
+            window.open(genshinlink["materials"][books[index]]["fandom"], "_blank");
+        });
         hover(book.previousElementSibling);
     });
     
@@ -126,6 +141,10 @@ function initializeData(){
     [...document.getElementsByClassName("boss")].forEach((bossDOM) =>{
         bossDOM.innerHTML = `${genshindb["materials"][boss]["name"]} <strong>x${bossAmounts}</strong>`;
         bossDOM.previousElementSibling.src = `${genshinimage["materials"][boss]["fandom"]}`;
+
+        bossDOM.previousElementSibling.addEventListener('click', () =>{
+            window.open(genshinlink["materials"][boss]["fandom"], "_blank");
+        });
         hover(bossDOM.previousElementSibling);
     });
     
@@ -133,6 +152,10 @@ function initializeData(){
     [...document.getElementsByClassName("crown")].forEach((crownDOM) =>{
         crownDOM.innerHTML = `${genshindb["materials"][crown]["name"]} <strong>x${crownAmounts}</strong>`;
         crownDOM.previousElementSibling.src = `${genshinimage["materials"][crown]["fandom"]}`;
+
+        crownDOM.previousElementSibling.addEventListener('click', () =>{
+            window.open(genshinlink["materials"][crown]["fandom"], "_blank");
+        });
         hover(crownDOM.previousElementSibling);
     });
 }
