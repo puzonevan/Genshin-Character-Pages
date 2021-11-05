0a00e100508ff0f44b23e563b1a59a2410e51b72
‘Evan <puzonevan@gmail.com>
2021-11-05 00:29:05 -0700

    data initialization materials

--- Liyue/Beidou/scripts/character.js
+++ Liyue/Beidou/scripts/character.js
@@ -1,6 +1,20 @@
+// Change variables below for different characters 
+// DO NOT CHANGE AMOUNTS FOR MATERIALS
 const name = "beidou";
+
 const stones = ["vajradaamethystsliver", "vajradaamethystfragment", "vajradaamethystchunk", "vajradaamethystgemstone", "noctilucousjade"];
+const stoneAmounts = [1, 9, 9, 6, 168];
 const collectable = ["treasurehoarderinsignia", "silverraveninsignia", "goldenraveninsignia"];
+const collectableAmounts = [18, 30, 36, 18, 66, 93];
+const uniqueCollectable = "lightningprism";
+const uniqueCollectableAmounts = 46;
+const books = ["teachingsofgold", "guidetogold", "philosophiesofgold"];
+const booksAmounts = [9, 65, 114];
+const boss = "dvalinssigh";
+const bossAmounts = 18;
+const crown = "crownofinsight";
+const crownAmounts = 3;
+
 const artifacts = ["gladiatorsfinale", "retracingbolide", "thunderingfury"];
 
 let genshindb = {};
@@ -55,17 +69,45 @@ function initializeData(){
     // Change Character Splash in Materials
     document.getElementById("character-splash").firstElementChild.src = genshinimage["characters"][`${name}`]["portrait"];
 
-    // Change Talent and Ascension Materials 
+    // Change Stone Materials 
     [...document.getElementsByClassName("stone")].forEach((stone, index) =>{
-        stone.innerHTML = `${genshindb["materials"][stones[index]]["name"]} <strong>x</strong>`;
+        stone.innerHTML = `${genshindb["materials"][stones[index]]["name"]} <strong>x${stoneAmounts[index]}</strong>`;
+        stone.previousElementSibling.src = `${genshinimage["materials"][stones[index]]["fandom"]}`;
     });
 
+    // Change Collectable Materials
     [...document.getElementsByClassName("collectable")].forEach((collect, index) =>{
         if(index < 3){
-            collect.innerHTML = `${genshindb["materials"][collectable[index]]["name"]} <strong>x</strong>`;
+            collect.innerHTML = `${genshindb["materials"][collectable[index]]["name"]} <strong>x${collectableAmounts[index]}</strong>`;
+            collect.previousElementSibling.src = `${genshinimage["materials"][collectable[index]]["fandom"]}`;
         } else{
-            collect.innerHTML = `${genshindb["materials"][collectable[index - 3]]["name"]} <strong>x</strong>`;
+            collect.innerHTML = `${genshindb["materials"][collectable[index - 3]]["name"]} <strong>x${collectableAmounts[index]}</strong>`;
+            collect.previousElementSibling.src = `${genshinimage["materials"][collectable[index - 3]]["fandom"]}`;
         }
+    });
+
+    // Change Unique Collectable Materials
+    [...document.getElementsByClassName("unique-collectable")].forEach((unique) =>{
+        unique.innerHTML = `${genshindb["materials"][uniqueCollectable]["name"]} <strong>x${uniqueCollectableAmounts}</strong>`;
+        unique.previousElementSibling.src = `${genshinimage["materials"][uniqueCollectable]["fandom"]}`;
+    });
+
+    // Change Book Materials
+    [...document.getElementsByClassName("book")].forEach((book, index) =>{
+        book.innerHTML = `${genshindb["materials"][books[index]]["name"]} <strong>x${booksAmounts[index]}</strong>`;
+        book.previousElementSibling.src = `${genshinimage["materials"][books[index]]["fandom"]}`;
+    });
+    
+    // Change Boss Materials
+    [...document.getElementsByClassName("boss")].forEach((bossDOM) =>{
+        bossDOM.innerHTML = `${genshindb["materials"][boss]["name"]} <strong>x${bossAmounts}</strong>`;
+        bossDOM.previousElementSibling.src = `${genshinimage["materials"][boss]["fandom"]}`;
+    });
+    
+    // Change Crown Materials
+    [...document.getElementsByClassName("crown")].forEach((crownDOM) =>{
+        crownDOM.innerHTML = `${genshindb["materials"][crown]["name"]} <strong>x${crownAmounts}</strong>`;
+        crownDOM.previousElementSibling.src = `${genshinimage["materials"][crown]["fandom"]}`;
     });
 }
 
