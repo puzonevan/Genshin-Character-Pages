6c1885844f80b2b0f758accd06d7cbed0514cd73
Evan Puzon <puzonevan@gmail.com>
2021-11-14 15:47:33 -0800

    card component

---
+++ cards.js
@@ -0,0 +1,48 @@
+const characters = [
+    {
+        name: "beidou", 
+        subtitle: "Uncrowned Lord of the Ocean"
+    },
+    {
+        name: "beidou", 
+        subtitle: "Uncrowned Lord of the Ocean"
+    },
+]
+
+const liyue = document.getElementById("characters-liyue");
+
+const createCard = (name, subTitle) =>{
+    const container = document.createElement("div");
+    container.setAttribute("class", "card");
+
+    const imageContainer = document.createElement("div");
+    imageContainer.setAttribute("class", "container-img");
+    const image = document.createElement("img");
+    image.setAttribute("src", `./assets/images/${name}-card.jpg`);
+    image.setAttribute("alt", `${name} character card`);
+    imageContainer.appendChild(image);
+
+    const infoContainer = document.createElement("div");
+    infoContainer.setAttribute("class", "container-info");
+    const characterTitle = document.createElement("h2");
+    characterTitle.setAttribute("class", "character-title");
+    const characterLink = document.createElement("a");
+    characterLink.setAttribute("href", `./Liyue/${name.charAt(0).toUpperCase() + name.slice(1)}/${name}.html`);
+    characterLink.textContent = name.charAt(0).toUpperCase() + name.slice(1);
+    characterTitle.appendChild(characterLink);
+    const characterSubTitle = document.createElement("h3");
+    characterSubTitle.setAttribute("class", "character-sub-title");
+    characterSubTitle.textContent = subTitle;
+    infoContainer.appendChild(characterTitle);
+    infoContainer.appendChild(characterSubTitle);
+
+    container.appendChild(imageContainer);
+    container.appendChild(infoContainer);
+
+    return container;
+}
+
+
+characters.forEach((character) =>{
+    liyue.appendChild(createCard(character.name, character.subtitle));
+});
