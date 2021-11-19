4fe5d7d78c4476ab0cd32823c8f649aa0d451017
‘Evan <puzonevan@gmail.com>
2021-11-18 20:33:23 -0800

    childe images

--- Liyue/Childe/scripts/character.js
+++ Liyue/Childe/scripts/character.js
@@ -1,7 +1,7 @@
 // Change variables below for different characters 
 // DO NOT CHANGE AMOUNTS FOR MATERIALS
 const name = "tartaglia";
-const image = ""
+const image = "./Childe/images/childe-main.png";
 
 ///////////////////////////////////////////////////////////////////////
 
