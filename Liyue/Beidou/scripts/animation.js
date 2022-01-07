5308e82de6ddcc54428f3ea5a385d42c66993395
‘Evan <puzonevan@gmail.com>
2022-01-06 21:35:02 -0800

    test own animations

--- Liyue/Beidou/scripts/animation.js
+++ Liyue/Beidou/scripts/animation.js
@@ -1,9 +1,29 @@
-function zoomInView(){
-    let bottomWindow = $(window).scrollTop
-}
+function zoomInView() {
+    // var $window = $(window);
+    // var bottom_of_window = $window.scrollTop() + $window.height();
+    let bottomWindow = window.scrollTop() + window.height();
+
+    
+    let randomQuote = document.getElementById("randomQuote");
+    let bottomObject = randomQuote.position.top() + randomQuote.outerHeight();
+
+    if(bottomWindow > bottomObject){
+        randomQuote.setAttribute("class", `${randomQuote.className} zoomIn`);
+    }
+    // $('#randomQuote').each(function(i) {
+    //   var $that = $(this);
+    //   var bottom_of_object = $that.position().top + $that.outerHeight();
+  
+    //   //if element is in viewport, add the animate class
+    //   if (bottom_of_window > bottom_of_object) {
+    //     $(this).addClass('zoomIn');
+    //   }
+    // });
+  }
 
 zoomInView();
 
 window.addEventListener("scroll", () =>{
     zoomInView();
 });
+
